b8d5f04449af20c857fec1b06d7cfade6148c5de
SaptakBhoumik <saptakbhoumik@gmail.com>
2022-01-10 15:42:15 +0530

    .

--- crawler/crawler.cpp
+++
@@ -1,6 +0,0 @@
-#include "gumbo.h"
-int main() {
-  GumboOutput* output = gumbo_parse("<h1>Hello, World!</h1>");
-  // Do stuff with output->root
-  gumbo_destroy_output(&kGumboDefaultOptions, output);
-}
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,33 +1,68 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-def crawl(url):
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-    }
-    f = requests.get(url, headers = headers)
-    link_lst = []
-    soup = BeautifulSoup(f.content, 'lxml')
-    links = soup.find_all('a')
-    for anchor in links:
-        urls=""
-        try:
-            href=anchor['href'] 
-            if "?" not in href:
-                if "https://" in href or "http://" in href:
-                    urls=href
-                else:
-                    urls = urljoin(url, anchor['href'])
-        except:pass
-        
-        link_lst.append(urls)
-    return link_lst
-def main():
-    x=crawl("https://en.wikipedia.org/")
-    for y in x:
-        print(y)
-        try:
-            z=crawl(y)
-            for i in z:print(i)
-        except:pass
-main()
+
+import json
+import requests    
+import re    
+from urllib.parse import urlparse    
+
+class PyCrawler(object):    
+    def __init__(self, starting_url):    
+        self.starting_url = starting_url    
+        self.visited = set()    
+
+    def get_html(self, url):    
+        try:    
+            html = requests.get(url)    
+        except Exception as e:    
+            print(e)    
+            return ""    
+        return html.content.decode('latin-1')    
+
+    def get_links(self, url):    
+        html = self.get_html(url)    
+        parsed = urlparse(url)    
+        base = f"{parsed.scheme}://{parsed.netloc}"    
+        links = re.findall('''<a\s+(?:[^>]*?\s+)?href="([^"]*)"''', html)    
+        for i, link in enumerate(links):    
+            if not urlparse(link).netloc:    
+                link_with_base = base + link    
+                links[i] = link_with_base    
+
+        return set(filter(lambda x: 'mailto' not in x, links))    
+
+    def extract_info(self, url):    
+        html = self.get_html(url)    
+        meta = re.findall("<meta .*?name=[\"'](.*?)['\"].*?content=[\"'](.*?)['\"].*?>", html)    
+        return dict(meta)    
+
+    def crawl(self, url):    
+        for link in self.get_links(url):    
+            if link in self.visited:    
+                continue    
+            self.visited.add(link)    
+            info = self.extract_info(link)    
+ 
+            if "?" not in link and ":" not in link:
+                with open("data.json",'r') as file:
+                    print(link)
+                    json_data=json.load(file)
+                    if link in json_data:
+                        #for some reason this is necessary
+                        try:json_data[link]["ref"]+=1
+                        except KeyError:json_data[link]={"ref":1,"des":info}
+                    else:
+                        json_data[link]={"ref":1,"des":info }
+                    json.dump(json_data, open("data.json",'w'),indent=4)
+                self.crawl(link)    
+
+    def start(self):    
+        self.crawl(self.starting_url)    
+
+if __name__ == "__main__":   
+    url= "https://github.com/"
+    with open("data.json",'r') as file:
+        json_data=json.load(file)
+        x=list(json_data.keys())
+        if len(x)!=0:url=x[-1]
+    print(url) 
+    crawler = PyCrawler(url)     
+    crawler.start()
